115371269e24ce67431e14c0905ccb86f5d2e20e
mshnschnko <misha321654987i@gmail.com>
2023-03-17 22:36:29 +0300

    lab is done

--- lab1-4/main.py
+++ lab1-4/main.py
@@ -1,7 +1,11 @@
 import pprint
 from research1 import *
 from research2 import *
+from research3 import *
+from research4 import *
 
 if __name__ == "__main__":
     lab1()
     pprint.pprint(lab2())
+    lab3()
+    lab4()
