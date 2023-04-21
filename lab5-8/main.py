75d7b782ffa7666f7ef7b64b7caafa4f283ab61e
mshnschnko <misha321654987i@gmail.com>
2023-04-21 18:06:54 +0300

    change main

--- lab5-8/main.py
+++ lab5-8/main.py
@@ -1,2 +1,11 @@
+from research5 import lab5
+from research6 import lab6
+from research7 import lab7
+from research8 import lab8
+
+
 if __name__ == "__main__":
-    pass
+    lab5()
+    lab6()
+    lab7()
+    lab8()
