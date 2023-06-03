2766b55ffbbf2619af678463104cd45331ce6251
mshnschnko <misha321654987i@gmail.com>
2023-06-03 22:35:18 +0300

    uncomment lines

--- lab9-10/src/main.m
+++ lab9-10/src/main.m
@@ -5,13 +5,13 @@ eps = 1e-4
 
 input_int = infsup(input - eps, input + eps)
 
-##data_int;
-##data_regr;
-##data_regr_reduce;
-##omegas;
-##regr_residuals;
-##regr_reduce_residuals;
-##mode_freq;
-##inform_set;
+data_int;
+data_regr;
+data_regr_reduce;
+omegas;
+regr_residuals;
+regr_reduce_residuals;
+mode_freq;
+inform_set;
 corridor_of_joint_dep;
 
